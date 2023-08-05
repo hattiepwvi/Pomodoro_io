c63db3b59befe5ebb1895fafd7c07022b48a11f3
eittah <pwhattie@gmail.com>
2023-08-05 12:38:21 +0800

    second commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
@@ -37,7 +37,7 @@ def start_count():
         timer_label.config(text="Break", fg=RED)
     elif reps % 2 == 0:
         count = short_break_sec
-        timer_label.config(text="Break", fg=PINK )
+        timer_label.config(text="Break", fg=PINK)
     else:
         count = work_sec
         timer_label.config(text="Work", fg=GREEN)
@@ -60,10 +60,10 @@ def count_down(count):
         timer = window.after(1000, count_down, count-1)
     else:
         start_count()
-        mark = ""
+        marks = ""
         for _ in range(math.floor(reps/2)):
-            mark += "️✔"
-            check_label.config(text=mark)
+            marks += "✔"
+        check_label.config(text=marks, fg=GREEN)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -72,7 +72,7 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-timer_label= Label(text="Timer", fg=GREEN, bg=YELLOW,font=(FONT_NAME, 50) )
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW,font=(FONT_NAME, 50))
 timer_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200,height=224, bg=YELLOW, highlightthickness=0)
@@ -82,11 +82,11 @@ timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT
 canvas.grid(column=1, row=1)
 
 start_button = Button(text="Start", highlightbackground=YELLOW, command=start_count)
-start_button.config(background=YELLOW)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset",highlightbackground=YELLOW, command=reset_timer)
 reset_button.grid(column=2, row=2)
+
 check_label = Label(fg=GREEN, bg=YELLOW)
 check_label.grid(column=1, row=3)
 
